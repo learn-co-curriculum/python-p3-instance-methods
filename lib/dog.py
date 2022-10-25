675df298e1bf77c82fc3e44bca5af296d2543c49
Ben Botsford <ben.botsford@flatironschool.com>
2022-10-25 10:45:46 -0500

    add solution

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 class Dog:
-    # Class body goes here
+    def bark(self):
+        print("Woof!")
 
-    #Instance method definition
-    pass
+    def sit(self):
+        print("The dog is sitting.")
+
--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 class Person:
-    # Class body goes here
-
-    #Instance method definition
-    pass
+    def talk(self):
+        print("Hello World!")
+    
+    def walk(self):
+        print("The person is walking.")
