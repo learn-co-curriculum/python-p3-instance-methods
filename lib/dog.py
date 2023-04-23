aec994c68062e2c4cf8640649dc976f6e836180d
Ricardo Guerra <RikkuG491@gmail.com>
2023-04-22 20:26:08 -0400

    Reversed changes in dog.py and person.py

--- lib/dog.py
+++ lib/dog.py
@@ -4,8 +4,4 @@ class Dog:
     # Class body goes here
 
     #Instance method definition
-    def bark(self):
-        print("Woof!")
-
-    def sit(self):
-        print("The dog is sitting.")
+    pass
--- lib/person.py
+++ lib/person.py
@@ -4,8 +4,4 @@ class Person:
     # Class body goes here
 
     #Instance method definition
-    def talk(self):
-        print("Hello World!")
-
-    def walk(self):
-        print("The person is walking.")
+    pass
