53cc174c2efd7410f5f95105afdb2819f2d889bc
Ricardo Guerra <RikkuG491@gmail.com>
2023-04-22 20:22:50 -0400

    Completed lab

--- lib/dog.py
+++ lib/dog.py
@@ -4,4 +4,8 @@ class Dog:
     # Class body goes here
 
     #Instance method definition
-    pass
+    def bark(self):
+        print("Woof!")
+
+    def sit(self):
+        print("The dog is sitting.")
--- lib/person.py
+++ lib/person.py
@@ -4,4 +4,8 @@ class Person:
     # Class body goes here
 
     #Instance method definition
-    pass
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
